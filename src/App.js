14e784d6440152ba6b6408afba8782fa64cb65c5
evo264714 <mh264714@gmail.com>
2023-03-24 21:09:46 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import './App.css';
 import Countries from './components/Countries/Countries';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
 
 
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <Countries></Countries>
+      <Footer></Footer>
     </div>
   );
 }
 
 
-
+ 
 // function LoadCountries(){
 //   const [countries, setCountries] = useState([]);
 
